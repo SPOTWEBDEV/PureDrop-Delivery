103e9f973ad26e83f605d1fa5801a8fc52bfdf45
SPOTWEBDEV <spotwebdev.com@gmail.com>
2025-01-10 13:24:32 +0100

    editing the the landing page

--- cart/index.php
+++ cart/index.php
@@ -42,125 +42,7 @@ include('../server/connection.php');
 
     <div class="boxed_wrapper">
 
-        <!-- main header -->
-        <header class="main-header">
-            <!-- header-lower -->
-            <div class="header-lower">
-                <div class="shape" style="background-image: url(<?php echo $domain ?>assets/images/shape/shape-1.png);"></div>
-                <div class="outer-box">
-                    <div class="logo-box">
-                        <figure class="logo"><a href="index-2.html"><img src="<?php echo $domain ?>assets/images/logo.png" alt=""></a></figure>
-                    </div>
-                    <div class="menu-area clearfix">
-                        <!--Mobile Navigation Toggler-->
-                        <div class="mobile-nav-toggler">
-                            <i class="icon-bar"></i>
-                            <i class="icon-bar"></i>
-                            <i class="icon-bar"></i>
-                        </div>
-                        <nav class="main-menu navbar-expand-md navbar-light">
-                            <div class="collapse navbar-collapse show clearfix" id="navbarSupportedContent">
-                                <ul class="navigation clearfix">
-                                    <li class="current"><a href="<?php echo $domain ?>">Home</a> </li>
-									<li><a href="<?php echo $domain ?>about/">About</a></li>
-                                    <li><a href="<?php echo $domain  ?>contact/">Contact</a></li>   
-                                </ul>
-                            </div>
-                        </nav>
-                    </div>
-                    <ul class="nav-right">
-                        <li class="search-box-outer">
-                            <div class="dropdown">
-                                <button class="search-box-btn" type="button" id="dropdownMenu3" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="far fa-search"></i></button>
-                                <div class="dropdown-menu search-panel" aria-labelledby="dropdownMenu3">
-                                    <div class="form-container">
-                                        <form method="post" action="https://azim.commonsupport.com/Acuasafe/blog.html">
-                                            <div class="form-group">
-                                                <input type="search" name="search-field" value="" placeholder="Search...." required="">
-                                                <button type="submit" class="search-btn"><span class="fas fa-search"></span></button>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="cart-box">
-                            <a href="cart.php"><i class="fal fa-shopping-cart"></i><span>3</span></a>
-                        </li>
-                        <li class="btn-box">
-                            <a href="index-2.html" class="theme-btn btn-one">Request A Quote</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
-            <!--sticky Header-->
-            <div class="sticky-header">
-                <div class="outer-box">
-                    <div class="logo-box">
-                        <figure class="logo"><a href="index-2.html"><img src="<?php echo $domain ?>assets/images/logo.png" alt=""></a></figure>
-                    </div>
-                    <div class="menu-area clearfix">
-                        <nav class="main-menu clearfix">
-                            <!--Keep This Empty / Menu will come through Javascript-->
-                        </nav>
-                    </div>
-                    <ul class="nav-right">
-                        <li class="search-box-outer">
-                            <div class="dropdown">
-                                <button class="search-box-btn" type="button" id="dropdownMenu4" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="far fa-search"></i></button>
-                                <div class="dropdown-menu search-panel" aria-labelledby="dropdownMenu4">
-                                    <div class="form-container">
-                                        <form method="post" action="https://azim.commonsupport.com/Acuasafe/blog.html">
-                                            <div class="form-group">
-                                                <input type="search" name="search-field" value="" placeholder="Search...." required="">
-                                                <button type="submit" class="search-btn"><span class="fas fa-search"></span></button>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="cart-box">
-                            <a href="shop.html"><i class="fal fa-shopping-cart"></i><span>3</span></a>
-                        </li>
-                        <li class="btn-box">
-                            <a href="index-2.html" class="theme-btn btn-one">Request A Quote</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </header>
-        <!-- main-header end -->
-
-        <!-- Mobile Menu  -->
-        <div class="mobile-menu">
-            <div class="menu-backdrop"></div>
-            <div class="close-btn"><i class="fas fa-times"></i></div>
-            
-            <nav class="menu-box">
-                <div class="nav-logo"><a href="index-2.html"><img src="<?php echo $domain ?>assets/images/logo-2.png" alt="" title=""></a></div>
-                <div class="menu-outer"><!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header--></div>
-                <div class="contact-info">
-                    <h4>Contact Info</h4>
-                    <ul>
-                        <li>Chicago 12, Melborne City, USA</li>
-                        <li><a href="tel:+8801682648101">+88 01682648101</a></li>
-                        <li><a href="mailto:info@example.com">info@example.com</a></li>
-                    </ul>
-                </div>
-                <div class="social-links">
-                    <ul class="clearfix">
-                        <li><a href="index-2.html"><span class="fab fa-twitter"></span></a></li>
-                        <li><a href="index-2.html"><span class="fab fa-facebook-square"></span></a></li>
-                        <li><a href="index-2.html"><span class="fab fa-pinterest-p"></span></a></li>
-                        <li><a href="index-2.html"><span class="fab fa-instagram"></span></a></li>
-                        <li><a href="index-2.html"><span class="fab fa-youtube"></span></a></li>
-                    </ul>
-                </div>
-            </nav>
-        </div><!-- End Mobile Menu -->
-        
+        <?php include('../includes/nav.php') ?>
 
 
         <!-- Page Title -->
@@ -301,110 +183,104 @@ include('../server/connection.php');
 
         <!-- main-footer -->
         <footer class="main-footer">
-            <div class="pattern-layer">
-                <div class="pattern-1" style="background-image: url(<?php echo $domain ?>assets/images/shape/shape-12.png);"></div>
-                <div class="pattern-2" style="background-image: url(<?php echo $domain ?>assets/images/shape/shape-13.png);"></div>
-                <div class="pattern-3" style="background-image: url(<?php echo $domain ?>assets/images/shape/shape-14.png);"></div>
+    <div class="pattern-layer">
+        <div class="pattern-1" style="background-image: url(assets/images/shape/shape-12.png);"></div>
+        <div class="pattern-2" style="background-image: url(assets/images/shape/shape-13.png);"></div>
+        <div class="pattern-3" style="background-image: url(assets/images/shape/shape-14.png);"></div>
+    </div>
+    <div class="auto-container">
+        <div class="footer-top clearfix">
+            <div class="line-shape" style="background-image: url(assets/images/shape/shape-11.png);"></div>
+            <div class="text pull-left">
+                <h2>Get <span>Pure Water</span> and <span>Bottled Water</span> Delivered to Your Doorstep</h2>
             </div>
-            <div class="auto-container">
-                <div class="footer-top clearfix">
-                    <div class="line-shape" style="background-image: url(<?php echo $domain ?>assets/images/shape/shape-11.png);"></div>
-                    <div class="text pull-left">
-                        <h2>Please <span>Call Us</span> to Take an Extraordinary Service</h2>
-                    </div>
-                    <div class="support-box pull-right">
-                        <a href="tel:7732253523"><i class="fas fa-phone"></i>(773) 225-3523</a>
+            <div class="support-box pull-right">
+                <a href="tel:7732253523"><i class="fas fa-phone"></i><?php echo $sitenumber ?></a>
+            </div>
+        </div>
+        <div class="widget-section">
+            <div class="row clearfix">
+                <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
+                    <div class="footer-widget logo-widget">
+                        <figure class="footer-logo"><a href="index-2.html"><img src="assets/images/footer-logo.png" alt="Water Company Logo"></a></figure>
+                        <div class="text">
+                            <p>We provide clean, pure water, delivered straight to your home. Enjoy fresh bottled water anytime.</p>
+                        </div>
+                        <div class="schedule-box">
+                            <h6>Open Hours:</h6>
+                            <ul class="list clearfix">
+                                <li>Mon - Sat: 9AM - 6PM</li>
+                                <li>Sunday: Closed</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-                <div class="widget-section">
-                    <div class="row clearfix">
-                        <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
-                            <div class="footer-widget logo-widget">
-                                <figure class="footer-logo"><a href="index-2.html"><img src="<?php echo $domain ?>assets/images/footer-logo.png" alt=""></a></figure>
-                                <div class="text">
-                                    <p>Nostrud exertation ullamco labor nisi aliquip commodo duis.</p>
-                                </div>
-                                <div class="schedule-box">
-                                    <h6>Open Hours:</h6>
-                                    <ul class="list clearfix">
-                                        <li>Mon - Sat: 9AM - 6PM.</li>
-                                        <li>Sunday: Closed</li>
-                                    </ul>
-                                </div>
-                            </div>
+                <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
+                    <div class="footer-widget contact-widget ml-70">
+                        <div class="widget-title">
+                            <h4>Our Address</h4>
                         </div>
-                        <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
-                            <div class="footer-widget contact-widget ml-70">
-                                <div class="widget-title">
-                                    <h4>Address</h4>
-                                </div>
-                                <div class="widget-content">
-                                    <ul class="info-list clearfix">
-                                        <li><i class="fal fa-map-marker-alt"></i>Flat 20, Reynolds Neck, FV77 8WS</li>
-                                        <li><i class="fal fa-phone"></i>Call Us: <a href="tel:3336660001">333-666-0001</a></li>
-                                        <li><i class="fal fa-envelope-open-text"></i><a href="mailto:info@example.com">info@example.com</a></li>
-                                    </ul>
-                                </div>
-                            </div>
+                        <div class="widget-content">
+                            <ul class="info-list clearfix">
+                                <li><i class="fal fa-map-marker-alt"></i><?php echo $siteaddress ?></li>
+                                <li><i class="fal fa-phone"></i>Call Us: <a href="tel:3336660001"><?php echo $sitenumber ?></a></li>
+                                <li><i class="fal fa-envelope-open-text"></i><a href="mailto:info@example.com"><?php echo $siteemail ?></a></li>
+                            </ul>
                         </div>
-                        <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
-                            <div class="footer-widget links-widget ml-70">
-                                <div class="widget-title">
-                                    <h4>Usefull Link</h4>
-                                </div>
-                                <div class="widget-content">
-                                    <ul class="links-list clearfix">
-                                        <li><a href="index-2.html">About Company</a></li>
-                                        <li><a href="index-2.html">Services</a></li>
-                                        <li><a href="index-2.html">How It Works</a></li>
-                                        <li><a href="index-2.html">Our Blog</a></li>
-                                        <li><a href="index-2.html">Contact Us</a></li>
-                                    </ul>
-                                </div>
-                            </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
+                    <div class="footer-widget links-widget ml-70">
+                        <div class="widget-title">
+                            <h4>Useful Links</h4>
                         </div>
-                        <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
-                            <div class="footer-widget subscribe-widget">
-                                <div class="widget-title">
-                                    <h4>Subscribe</h4>
-                                </div>
-                                <div class="widget-content">
-                                    <p>Lorem ipsum dolor sit amet, consect adipisicing elit sed do eiusmod.</p>
-                                    <div class="form-inner">
-                                        <form action="https://azim.commonsupport.com/Acuasafe/contact.html" method="post">
-                                            <div class="form-group">
-                                                <input type="email" name="email" placeholder="Your Email" required="">
-                                                <button type="submit"><i class="far fa-long-arrow-alt-right"></i></button>
-                                            </div>
-                                        </form>
+                        <div class="widget-content">
+                            <ul class="links-list clearfix">
+                                <li><a href="index-2.html">About Us</a></li>
+                                <li><a href="index-2.html">Products</a></li>
+                                <li><a href="index-2.html">How It Works</a></li>
+                                <li><a href="index-2.html">Our Blog</a></li>
+                                <li><a href="index-2.html">Contact Us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12 footer-column">
+                    <div class="footer-widget subscribe-widget">
+                        <div class="widget-title">
+                            <h4>Subscribe for Offers</h4>
+                        </div>
+                        <div class="widget-content">
+                            <p>Stay updated on special offers and new water products directly from us.</p>
+                            <div class="form-inner">
+                                <form action="https://azim.commonsupport.com/Acuasafe/contact.html" method="post">
+                                    <div class="form-group">
+                                        <input type="email" name="email" placeholder="Your Email" required="">
+                                        <button type="submit"><i class="far fa-long-arrow-alt-right"></i></button>
                                     </div>
-                                </div>
+                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="footer-bottom">
-                <div class="auto-container">
-                    <div class="bottom-inner">
-                        <div class="copyright">
-                            <p><a href="index-2.html">Acuasafe</a> &copy; 2021 All Right Reserved</p>
-                        </div>
-                        <ul class="social-links clearfix">
-                            <li><a href="index-2.html"><i class="fab fa-facebook-f"></i></a></li>
-                            <li><a href="index-2.html"><i class="fab fa-twitter"></i></a></li>
-                            <li><a href="index-2.html"><i class="fab fa-google-plus-g"></i></a></li>
-                            <li><a href="index-2.html"><i class="fab fa-linkedin-in"></i></a></li>
-                        </ul>
-                        <ul class="footer-nav clearfix">
-                            <li><a href="index-2.html">Terms of Service</a></li>
-                            <li><a href="index-2.html">Privacy Policy</a></li>
-                        </ul>
-                    </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="auto-container">
+            <div class="bottom-inner">
+                <div class="copyright">
+                    <p><a href="index-2.html">Pure Water Co.</a> &copy; 2025 All Rights Reserved</p>
                 </div>
+                
+                <ul class="footer-nav clearfix">
+                    <li><a href="index-2.html">Terms of Service</a></li>
+                    <li><a href="index-2.html">Privacy Policy</a></li>
+                </ul>
             </div>
-        </footer>
-        <!-- main-footer end -->
+        </div>
+    </div>
+</footer>
 
 
 
